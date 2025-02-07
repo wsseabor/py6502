ca0026cad6a9137072b881325d49ee89813cee9a
Schuyler Seaborn <wsseabor@gmail.com>
2025-02-07 17:08:10 -0500

    Added methods to read bytes and words

--- py6502/memory.py
+++ py6502/memory.py
@@ -1,22 +1,22 @@
 class Memory:
-    def __init__(self, size: int = 65536) -> None:
+    def __init__(self, size: int = 65535) -> None:
         """
         Memory class for the 6502 processor. Initializes a 'protected' empty list where each index is of uint8 data type
         Sets a list of 'memory' that extends to max value
 
-        @Param size: size of memory, 2^16 or 65536
+        @Param size: size of memory, 2^16 - 1 or 65535
         @Return: None
 
         """
         self.size = size
         self._mem = [0] * self.size
 
-    def read(self, addr: int) -> int:
+    def read_byte(self, addr: int) -> int:
         """
-        Reads an address from the memory array
+        Reads a byte address from the memory array
 
         @Param addr: address index to read
-        @Return: int (address of memory to be retrieved)
+        @Return: int (byte in memory to be retrieved)
         """
 
         #If not between 0 and self.size, raise value error
@@ -24,7 +24,22 @@ class Memory:
             raise ValueError("Memory address is not valid")
         else:
             return self._mem[addr]
+        
+    def read_word(self, addr: int) -> int:
+        """
+        Reads a word (2 byte) address from the memory array
 
+        Accounting for little-endian, the first byte read will be the low_byte, high_byte will be second byte 
+        to be read, concatenate them both by shifting the high byte left by 8 and using bitwise OR to finish
+
+        @Param addr: address index to read
+        @Return: int (word in memory to be retrieved)
+        """
+
+        low_byte = self.read_byte(addr)
+        high_byte = self.read_byte(addr + 1)
+        return (high_byte >> 8) | low_byte
+        
     def write(self, addr: int, value: int) -> None:
         """
         Writes to a specific memory address
