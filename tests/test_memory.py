567021f6d1b0bb4ba3a98c3c341017adb8d14c56
Schuyler Seaborn <wsseabor@gmail.com>
2025-02-08 12:12:04 -0500

    Added further memory testing

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -21,8 +21,9 @@ class MemTest(unittest.TestCase):
         self.size_hex = 0xFFFF
         
         print("\n --- Testing memory initalization ---")
+        print(f"Memory size: {self.mem.size}")
+        print(f"Expected size: {self.size}")
         self.assertEqual(self.mem.size, self.size)
-        self.assertEqual(self.mem.size, self.size_hex)
 
     def test_read_word(self) -> None:
         """
@@ -40,6 +41,17 @@ class MemTest(unittest.TestCase):
 
         print(f"Wrote bytes: ${self.mem.read_byte(0x0000):02X} (low) ${self.mem.read_byte(0x0001):02X} (high)")
         res = self.mem.read_word(0x0000)
+        print(f"Read word: ${res:04X}")
+        print(f"Expected: $1234")
+        self.assertEqual(res, 0x1234)
+
+        #Test case two
+        print("\nTest case two: Read word at end of page")
+        self.mem.write(0x00FF, 0x34)
+        self.mem.write(0x0100, 0x12)
+        
+        print(f"Wrote bytes: ${self.mem.read_byte(0x00FF):02X} (low) ${self.mem.read_byte(0x0100):02X} (high)")
+        res = self.mem.read_word(0x00FF)
         print(f"Read word: ${res:04X}")
         print(f"Expected: $1234")
         self.assertEqual(res, 0x1234)
