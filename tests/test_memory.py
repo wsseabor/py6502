54818ece2e3617aaf7af231b77ddaa0b4ff2fbf4
Schuyler Seaborn <wsseabor@gmail.com>
2025-02-09 19:10:46 -0500

    Finish memory test file

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -17,12 +17,14 @@ class MemTest(unittest.TestCase):
     
         @Return: None
         """
+
+        #Test case one
+        print(f"Test case one: Default memory init")
         self.size = 65535
-        self.size_hex = 0xFFFF
+        self.size_hex = hex(self.size)
         
-        print("\n --- Testing memory initalization ---")
-        print(f"Memory size: {self.mem.size}")
-        print(f"Expected size: {self.size}")
+        print(f"\nMemory size: {self.mem.size}")
+        print(f"\nExpected size: {self.size}")
         self.assertEqual(self.mem.size, self.size)
 
     def test_read_word(self) -> None:
@@ -32,7 +34,6 @@ class MemTest(unittest.TestCase):
 
         @Return: None
         """
-        print("\n --- Testing read_word ---")
 
         #Test case one
         print("\nTest case One: Read word")
@@ -56,6 +57,25 @@ class MemTest(unittest.TestCase):
         print(f"Expected: $1234")
         self.assertEqual(res, 0x1234)
 
+        #Test case three 
+        print("\nTest case three: Read / write zero value")
+        self.mem.write(0x0400, 0x00)
+        self.mem.write(0x0401, 0x00)
+        print(f"Wrote bytes: ${self.mem.read_byte(0x0400):02X} (low) ${self.mem.read_byte(0x0401):02X} (high)")
+        res = self.mem.read_word(0x0400)
+        print(f"Read word: ${res:04X}")
+        print(f"Expected: 0000")
+        self.assertEqual(res, 0x00)
+
+        #Test case four
+        print(f"Test case four: Read / write maximum value")
+        self.mem.write(0x0500, 0xFF)
+        self.mem.write(0x0501, 0xFF)
+        print(f"Wrote bytes: ${self.mem.read_byte(0x0500):02X} (low) ${self.mem.read_byte(0x0501):02X} (high)")
+        res = self.mem.read_word(0x0500)
+        print(f"Read word: ${res:04X}")
+        print(f"Expected: $FFFF")
+        self.assertEqual(res, 0xFFFF)
 
 
 if __name__ == "__main__":
